feef056a37cf4b25d1179e2de3d34be439975b6d
hiramr97 <hiram.rodriguez97@icloud.com>
2023-06-05 11:48:06 -0400

    added footer

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <div className="mt-10 min-w-screen bg-slate-700 h-20 bottom-0">
+        <div className="w-[60%] mx-auto flex text-slate-300">
+          <p>All data is gathered from Unite API and Unite API</p>
+        </div>
+      </div>
+    );
+  }
