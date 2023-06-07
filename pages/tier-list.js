e16a6ae50bba4207c2d931e21c52c91e6ea6e59c
hiramr97 <hiram.rodriguez97@icloud.com>
2023-06-07 14:08:39 -0400

    added tiers with default names and colors on tier list page

--- pages/tier-list.js
+++ pages/tier-list.js
@@ -5,11 +5,30 @@ import Link from "next/link";
 export default function TierList() {
   return (
     <div className="min-h-screen min-w-screen">
-      <div></div>
+      <div className="text-white mx-auto w-[60%] rounded-md bg-slate-800 border-slate-800 border-2 shadow-md shadow-black h-28 mb-6">
+        <p className="bg-red-400 w-1/12 h-full rounded-l-md">S</p>
+        <div></div>
+      </div>
+      <div className="text-white mx-auto w-[60%] rounded-md bg-slate-800 border-slate-800 border-2 shadow-md shadow-black h-28 mb-6">
+        <p className="bg-orange-300 w-1/12 h-full rounded-l-md">A</p>
+        <div></div>
+      </div>
+      <div className="text-white mx-auto w-[60%] rounded-md bg-slate-800 border-slate-800 border-2 shadow-md shadow-black h-28 mb-6">
+        <p className="bg-amber-300 w-1/12 h-full rounded-l-md">B</p>
+        <div></div>
+      </div>
+      <div className="text-white mx-auto w-[60%] rounded-md bg-slate-800 border-slate-800 border-2 shadow-md shadow-black h-28 mb-6">
+        <p className="bg-lime-300 w-1/12 h-full rounded-l-md">C</p>
+        <div></div>
+      </div>
+      <div className="text-white mx-auto w-[60%] rounded-md bg-slate-800 border-slate-800 border-2 shadow-md shadow-black h-28 mb-6">
+        <p className="bg-green-400 w-1/12 h-full rounded-l-md">D</p>
+        <div></div>
+      </div>
 
       <div className="flex flex-wrap justify-evenly w-[61%] mx-auto">
         {data.map((pokemon, id) => {
-            console.log(data[pokemon.name])
+          console.log(data[pokemon.name]);
           return (
             <div
               href={`/pokemon/${pokemon.name.toLowerCase()}`}
